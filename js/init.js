82e6879a0b97609f199b7a287da5b6f327155c10
Devon Griffith <70294643+DevonGriffithLearn@users.noreply.github.com>
2020-11-05 21:44:06 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -6,3 +6,32 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(".sidebar-dropdown > a").click(function() {
+  $(".sidebar-submenu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .parent()
+      .removeClass("active");
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sidebar-submenu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
+
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+});
